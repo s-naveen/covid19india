c853ee1ae33f578942c3679d74457225f0181a3d
Naveen S <coolnaveen1996@gmail.com>
2020-03-22 00:10:43 +0530

    Date Changes

--- script.js
+++ script.js
@@ -74,10 +74,10 @@ function(result) {
 
 
 function constructTweetButton() {
-    let current_date = new Date().toLocaleString('en-US');
+    let current_date = new Date().toLocaleString('en-IN');
     let active_delta = key_values.confirmeddelta - key_values.recovereddelta
     let active_delta_status = active_delta >= 0? "increased" : "decreased";
-    const tweet_content = `COVID-19 India : 📊 as of ${current_date}
+    const tweet_content = `COVID-19 India : 📊 as of ${current_date} IST
 Total Confirmed : ${total.confirmed}
 Total Recovered : ${total.recovered}
 Total Deceased. : ${total.deaths}
